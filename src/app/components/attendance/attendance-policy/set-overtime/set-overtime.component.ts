dbeac1d0252e891b836b9be6596adfaebdde888f
Phattaraphon61 <phattaraphon.pe.61@ubu.ac.th>
2023-03-14 19:01:25 +0700

    setpolicy_overtime

--- src/app/components/attendance/attendance-policy/set-overtime/set-overtime.component.ts
+++ src/app/components/attendance/attendance-policy/set-overtime/set-overtime.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppConfig } from 'src/app/config/config';
+import { InitialCurrent } from 'src/app/config/initial_current';
+import { OvertimeModels } from 'src/app/models/attendance/overtime';
+import { OTServices } from 'src/app/services/attendance/rateot.service';
 
 @Component({
   selector: 'app-set-overtime',
@@ -6,13 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./set-overtime.component.scss']
 })
 export class SetOvertimeComponent implements OnInit {
-  policy = [
-    { name: 'OTD - OT (Daily work)', code: "OTD" },
-    { name: 'OTM - OT (Monthly)', code: "OTM" },
-  ];
-  constructor() { }
-
+  policy: any = []
+  check: boolean = false;
+  constructor(
+    private OtService: OTServices,
+    private router: Router
+  ) { }
+  public initial_current: InitialCurrent = new InitialCurrent();
+  doGetInitialCurrent() {
+    this.initial_current = JSON.parse(localStorage.getItem(AppConfig.SESSIONInitial) || '{}');
+    if (!this.initial_current) {
+      this.router.navigateByUrl('');
+    }
+  }
   ngOnInit(): void {
+    this.doGetInitialCurrent();
+    this.doLoadOt()
+  }
+
+  doLoadOt() {
+    var tmp = new OvertimeModels();
+    this.OtService.ot_get(tmp).then(async (res) => {
+      await res.forEach((element: OvertimeModels) => {
+        this.policy.push(
+          {
+            name: this.initial_current.Language == "EN" ? element.rateot_name_en : element.rateot_name_th,
+            code: element.rateot_code
+          }
+        )
+      });
+      this.check = true;
+    });
   }
 
 }
