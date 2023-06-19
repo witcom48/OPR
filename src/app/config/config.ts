d1b7041bd4ea5401842df69a51df5de75d861bdc
witcom48 <witcom48@hotmail.com>
2023-06-19 09:49:31 +0700

    update project manage

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,7 +2,7 @@ export class AppConfig {
   constructor() {
 
   }
-  // UrlApi: string = "http://localhost:32208"
+  //UrlApi: string = "http://localhost:32208"
   UrlApi: string = "http://161.82.218.95:8806"
 
   ApiMainModule: string = this.UrlApi + "/BpcOpr.svc/BpcOpr";
