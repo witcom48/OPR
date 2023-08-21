dffa2abc3ba3fc09c2a4a44f8708fc1c16ccd48b
witcom48 <witcom48@hotmail.comgit>
2023-08-21 10:15:35 +0700

    calculate time by job, document request

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -4,8 +4,8 @@ export class AppConfig {
   constructor() {
 
   }
-  // UrlApi: string = "http://localhost:32208"
-  UrlApi: string = "http://161.82.218.95:8806"
+   UrlApi: string = "http://localhost:32208"
+  //UrlApi: string = "http://161.82.218.95:8806"
 
 
 
